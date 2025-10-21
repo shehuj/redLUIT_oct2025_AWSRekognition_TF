4c8376a1a9a2ddd8b437373607f22d6f975d5a8c
Jenom <119628596+shehuj@users.noreply.github.com>
2025-10-21 10:50:13 -0500

    added s3 client

--- lambda/lambda_handler_prod.py
+++ lambda/lambda_handler_prod.py
@@ -12,6 +12,8 @@ from urllib.parse import unquote_plus
 # Initialize AWS clients
 rekognition_client = boto3.client('rekognition')
 dynamodb = boto3.resource('dynamodb')
+s3_client = boto3.client('s3')
+bucket_name = os.environ.get('S3_BUCKET', 'pixel-learning-rekognition-images-7bgawsey')
 
 # Get DynamoDB table name from environment variable
 DYNAMODB_TABLE = os.environ.get('DYNAMODB_TABLE', 'prod_results')
