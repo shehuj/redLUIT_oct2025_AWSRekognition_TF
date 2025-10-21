0e778c6fbb9efa2f3024b14088324170625c0a34
Jenom <119628596+shehuj@users.noreply.github.com>
2025-10-21 11:19:32 -0500

    Update main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -32,10 +32,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "images" {
 resource "aws_s3_bucket_public_access_block" "images" {
   bucket = aws_s3_bucket.images.id
   
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # Lifecycle rule to clean up old images (optional)
